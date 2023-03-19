0660cee0642cef24f3f204ac1a36e7143b2a3e83
Gleidson Domiciano <58370373+domi900@users.noreply.github.com>
2023-03-19 04:02:37 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-var lista = []
-var cont = 0
+var lista = [];
+var cont = 0;
 
-function adicionar(){
-    var textoNometarefa = window.document.getElementById("textoNomeTarefa")
-    if(textoNometarefa.value) {   
-        var tarefas = window.document.getElementById("tarefas")
-        var textoNometarefa = window.document.getElementById("textoNomeTarefa")
-        lista.push(`<li>${textoNometarefa.value}</li>`)
-        tarefas.innerHTML += lista[cont]
-        cont += 1
-    }else{
-        window.alert("digite algo")
-        }
-    }
+function adicionar() {
+  var textoNomeTarefa = document.getElementById("textoNomeTarefa");
+  var nomeTarefa = textoNomeTarefa.value.trim();
+  
+  if (nomeTarefa !== "") { 
+    var tarefas = document.getElementById("tarefas");
+    var novaTarefa = {nome: nomeTarefa, id: cont};
+    lista.push(novaTarefa); 
+    tarefas.innerHTML += `<li id="${cont}">${nomeTarefa}</li>`;
+    cont++;
+    textoNomeTarefa.value = "";
+  } else {
+    window.alert("Digite algo para adicionar."); 
+  }
+}
 
 function remover() {
-        var textoNometarefa = window.document.getElementById("textoNomeTarefa"); // pega o elemento input com o ID "textoNomeTarefa"
-        if (textoNometarefa.value) { // verifica se o valor do input é vazio
-            var tarefas = window.document.getElementById("tarefas"); // pega a lista de tarefas com o ID "tarefas"
-            var lista1 = lista.slice(); // cria uma cópia da lista original
-            var elemento = `<li>${textoNometarefa.value}</li>`; // cria um novo elemento de lista a partir do valor do input
-            var indice = lista1.indexOf(elemento); // verifica o índice do elemento na cópia da lista
-            if (indice >= 0) { // verifica se o elemento foi encontrado na cópia da lista
-                lista1.splice(indice, 1); // remove o elemento da cópia da lista
-                var novatarefas = tarefas.innerHTML.replace(elemento, ""); // remove o elemento da lista de tarefas
-                tarefas.innerHTML = novatarefas; // atualiza a lista de tarefas na página
-                lista = lista1; // atualiza a lista original
-                cont--; // decrementa o contador de tarefas
-            } else {
-                window.alert("Elemento não encontrado na lista."); // exibe mensagem de erro se o elemento não for encontrado na lista
-            }
-        } else {
-            window.alert("Digite algo para remover."); // exibe mensagem de erro se o input estiver vazio
+    var textoNomeTarefa = document.getElementById("textoNomeTarefaRemover");
+    var nomeTarefa = textoNomeTarefa.value.trim();
+    
+    if (nomeTarefa !== "") { 
+      var tarefas = document.getElementById("tarefas");
+      var indice = -1; 
+      
+      
+      for (var i = 0; i < lista.length; i++) {
+        if (lista[i].nome === nomeTarefa) {
+          indice = i;
+          break;
         }
+      }
+      
+      if (indice >= 0) { 
+        var idTarefa = lista[indice].id;
+        lista.splice(indice, 1);
+        var tarefaRemover = document.getElementById(idTarefa);
+        tarefas.removeChild(tarefaRemover);
+        textoNomeTarefa.value = "";
+        cont--;
+      } else {
+        window.alert("Tarefa não encontrada na lista."); 
+      }
+    } else {
+      window.alert("Digite algo para remover."); 
     }
+  }
+  
